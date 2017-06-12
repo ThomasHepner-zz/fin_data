432c032a95d61c1c39fc7a4312d4f0eb6ec55150
Thomas Hepner <thepner32@gmail.com>
2017-06-11 21:54:38 -0400

    Added routes for remote call

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,11 @@
 class ReportsController < ApplicationController
   def new
+  	@data_options = [{ type: 'UF', value: 1 }, { type: 'USD', value: 2}]
   end
 
   def show
+  end
+
+  def remote_lookup
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   root 'reports#new'
 
+  get 'reports/remote_lookup', to: 'reports#remote_lookup'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
