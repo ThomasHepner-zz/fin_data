e9d971d0b8c5b7d80d8f3d3dc2e2321668c092f3
Thomas Hepner <thepner32@gmail.com>
2017-06-13 13:30:08 -0400

    Added date validation

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,6 +1,26 @@
 var init = function() {
 	$('#date_from').datepicker();
 	$('#date_to').datepicker();
+
+	$.validator.addMethod('dateFromBeforeDateTo', function () {
+		var field_1 = $('input[name="date_from"]').val(),
+				field_2 = $('input[name="date_to"]').val();
+
+		if (field_1.length == 0 || field_2.length == 0) return false;
+
+		var date_from = new Date(field_1),
+				date_to = new Date(field_2);
+
+		return date_from < date_to;
+	}, 'The beginning date must be earlier than the ending date of the report');
+
+	$('#reportForm').validate({
+		rules: {
+			date_to: {
+				dateFromBeforeDateTo: true
+			}
+		}
+	});
 };
 
 init();
